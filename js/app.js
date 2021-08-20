b26b9e0754cd5b99be64ea65542c5b27132cbe6e
nastaran poursharif <nastaran.p.sh.77@gmail.com>
2021-08-21 02:54:34 +0430

    js form values

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 // class
 
 // listeners
+listeners();
+function listeners() {
+     document.querySelector("#submitBtn").addEventListener("click", search);
+}
 
 // functions
+function search(e) {
+    e.preventDefault()
+    const newsName = document.querySelector('#news-name').value
+    const country = document.querySelector('#country').value
+    const category = document.querySelector('#category').value
+
+    if (newsName !== '' || country !== '' || category !=='' ) {
+        console.log('true');
+    } else {
+        console.log('false');
+    }
+}
